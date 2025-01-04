625a55b2bded7856e232d39fba68e22311630adf
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-01-04 19:13:07 +0000

    feat: define types for HTTP methods, middleware, and route handling

--- types.ts
+++ types.ts
@@ -1,1 +1,15 @@
-import {} from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
+import { RoutifyRequest} from './request'
+import { RoutifyResponse } from './response'
+
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+export type NextFunction = () => Promise<void>;
+export type Middleware = (req: RoutifyRequest, res: RoutifyResponse, next: NextFunction) => Promise<void>;
+export type RouteHandler = (req: RoutifyRequest, res: RoutifyResponse) => Promise<void>;
+
+export interface Route {
+  method: HttpMethod;
+  path: string;
+  handler: RouteHandler;
+  middlewares: Middleware[];
+}
